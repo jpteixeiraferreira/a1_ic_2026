8248026896b615e62a919d97532e88631100a762
João Pedro <jpteixeiraferreira@gmail.com>
2026-05-25 17:21:37 -0300

    Início do projeto

---
+++ src/main/java/jp/algoritmosbusca/AlgoritmosBusca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jp.algoritmosbusca;
+
+/**
+ *
+ * @author jpcms
+ */
+public class AlgoritmosBusca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
